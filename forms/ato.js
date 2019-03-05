dea76a328152e1fdb52e3f866d1f72ded79fa7df
luke <luke>
2019-03-05 16:57:25 -0400

    atoj function stub

--- forms/ato.js
+++ forms/ato.js
@@ -6,19 +6,27 @@ var arr3 = [];
 
 for (i=0; i<larr;i=i+2)
 {
-arr2.push(arr[i]);
+    arr2.push(arr[i]);
 
  
 }
 for (j=1; j<larr;j=j+2)
 {
-arr3.push(arr[j]);
+    arr3.push(arr[j]);
 }
+
 
 for (k=0; k<arr2.length; k++)
 {
-let x = arr2[k];
-obj[x] =  arr3[k];
+    let x = arr2[k];
+    obj[x] =  arr3[k];
+}
+
+function arrayToDict(a) // a is an array [], returns dictionary/object
+{
+    let o = {}
+    // TODO
+    return o;
 }
 
 console.log(arr2); // ["a", "b", "c"]
