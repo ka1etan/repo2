427e89d86451af926b137cf3b379772172a50415
ka1etan <ka1etan@wp.pl>
2019-03-30 18:15:29 +0100

    Created simple audio controls.

---
+++ ent/audio.js
@@ -0,0 +1,45 @@
+function createaudio() {
+  let newtrack = new Audio("nitro_silwerk.mp3");
+  document.getElementById("player").innerHTML = newtrack;
+
+  newtrack.addEventListener('suspend', (event) => {
+    console.log('Data loading has been suspended.');
+  });
+
+  let bplay = document.getElementById("play");
+  let bpause = document.getElementById("stop");
+  let bmute = document.getElementById("mute");
+  let brew = document.getElementById("rewind");
+  bplay.addEventListener("click", playtrack);
+  bpause.addEventListener("click", pausetrack);
+  bmute.addEventListener("click", mutetrack);
+  brew.addEventListener("click", rewtrack);
+
+  function playtrack() {
+    newtrack.play();
+  }
+
+  function pausetrack() {
+    newtrack.pause();
+  }
+
+  function mutetrack() {
+
+    if (newtrack.muted == true) {
+      newtrack.muted = false;
+    } else newtrack.muted = true;
+  }
+
+  function rewtrack() {
+
+    newtrack.currentTime = 0;
+  }
+
+  document.getElementById("dura").innerHTML = "Duration: " + newtrack.duration;
+}
+
+function init() {
+  createaudio();
+}
+
+window.addEventListener("load", init);
