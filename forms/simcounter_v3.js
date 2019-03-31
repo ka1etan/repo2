07f17b81d12dfb82bd4e604a0ac42c1d4935aa2c
luke <luke>
2019-03-31 12:38:26 -0400

    numCheckObj3 - new version

--- forms/simcounter_v3.js
+++ forms/simcounter_v3.js
@@ -30,6 +30,29 @@ function numCheckObj(x) {
 
 }
 
+/*
+
+array: index (number) -> value  (number, object, string)
+
+array[index]
+0..2, 10 -> 0..10, 10005 -> 0..10005
+10005
+0..10005
+
+
+associative array:  key (anything) -> value (anything)
+//key => number => hash_map
+
+assoc_array[key]
+0..2, 10 -> 0,1,2,10 , 10005 -> 0,1,2,10005
+
+a. array of objects (not counters)
+b. linear search by key
+c. associative array 
+
+
+*/
+
 function numCheckObj2(x) {
 
     let z = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -50,14 +73,48 @@ function numCheckObj2(x) {
     }
 
     return object;
+}
 
+function numCheckObj3(x) {
+
+
+    //let z = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    //let z = [];
+    let counters = [ 
+        {name: "0", count: 0}, 
+        {name: "1", count: 0}, 
+        {name: "2", count: 0}, 
+        {name: "3", count: 0}, 
+        {name: "4", count: 0}, 
+        {name: "5", count: 0}, 
+        {name: "6", count: 0}, 
+        {name: "7", count: 0}, 
+        {name: "8", count: 0}, 
+        {name: "9", count: 0}, 
+    ];
+
+
+    for (let i = 0; i < x.length; i++) 
+    {
+        
+        if (0 <= x[i] && x[i] < counters.length) 
+        {
+            // ??
+
+        }
+
+    }
+
+    return object;
 }
 
 let x = [1,1,1,2,4,6,9,1,4,6,0,0,7,4];
 
-let check = numCheck(x);
-let check2 = numCheckObj(x);
-let check3 = numCheckObj2(x);
-console.log(check);
-console.log(check2);
-console.log(check3);
+// let check = numCheck(x);
+// let check2 = numCheckObj(x);
+// let check3 = numCheckObj2(x);
+// console.log(check);
+// console.log(check2);
+// console.log(check3);
+
+console.log( numCheckObj3(x) )
