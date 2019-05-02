a879000857203a49616f79d89c1d939c0e560e80
ka1etan <ka1etan@wp.pl>
2019-05-03 00:28:05 +0200

    arrows

---
+++ forms/arrows.js
@@ -0,0 +1,53 @@
+function modifyArray(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let d = nums[i] % 2;
+        if (d == 0) {
+            nums[i] = nums[i] * 2;
+        } else {
+            nums[i] = nums[i] * 3;
+        }
+
+    }
+    return nums;
+
+}
+
+// function modifyArray2(nums) {
+   
+//     //let buf2 = [];
+//     for (let i = 0; i < nums.length; i++){
+//         // let d = nums[i] % 2;
+//         // const f = nums[i] => (nums[i] % 2) === 0 ? nums[i] = nums[i] * 2 : nums[i] = nums[i] * 3;
+//         // if (d==0) {
+           
+//         //     nums[i] = nums[i] * 2;
+//         // } else {
+            
+//         //     nums[i] = nums[i] * 3;
+//         // }
+        
+//     }
+//     //return buf;
+//     return nums;
+//     //console.log(buf2);
+// }
+
+// function div(z){
+//     for (let i = 0; i < z.length; i++){
+//         const f = z[i] => (z[i] % 2) === 0 ? "even" : z[i]*3;
+
+//     }
+// }
+
+let nums = [1,2,3,4,5];
+let nums2 = [1,2,3,4,5];
+
+const f = x => (x % 2) === 0 ? "even" : "odd";
+
+console.log(modifyArray(nums));
+console.log("");
+// console.log(modifyArray2(nums2));
+console.log(f(3));
+// console.log(div(3));
+
+
