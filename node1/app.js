4f5eaacb722fdb106aa3282f5363fe85e7c8705f
luke <luke>
2019-05-06 18:43:58 -0400

    Example of nodejs program reading from local file instead of standard input (stdin) - see templateLiterals.js

---
+++ node1/app.js
@@ -0,0 +1,15 @@
+var m = "Hi"
+console.log(m)
+
+/*
+
+*/
+
+var x = {}
+
+x.filip = {}
+
+x["filip"] = {}
+
+var name = "filip"
+x[name] = {}
---
+++ node1/templateLiterals.js
@@ -0,0 +1,56 @@
+var fs = require("fs");
+var stream = fs.createReadStream("in.txt");
+// var stream = process.stdin;
+
+// process.stdin.resume();
+// process.stdin.setEncoding('ascii');
+stream.resume();
+stream.setEncoding("ascii");
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+//process.stdin.
+stream.on('data', function (data) {
+    input_stdin += data;
+});
+
+//process.stdin.
+stream.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+function Person(initialAge){
+    // Add some more code to run some checks on initialAge
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console
+   console.log("You are young");
+
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+   };
+}
+
+function main() {
+
+
+var T=parseInt(readLine());
+for(i=0;i<T;i++){
+    var age=parseInt(readLine());
+    var p=new Person(age);
+    p.amIOld();
+    for(j=0;j<3;j++){
+        p.yearPasses();
+        
+    }
+    p.amIOld();
+    console.log("");   
+    }
+}
