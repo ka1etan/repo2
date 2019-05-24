94253f446d262526d7b1dd0388e9b8dc75c343b9
ka1etan <ka1etan@wp.pl>
2019-05-24 16:52:58 +0200

    isPalindrome function

--- node1/arrays.js
+++ node1/arrays.js
@@ -5,6 +5,29 @@
 // abba
 // oko
 // abcddcba
-function isPalindrome(s)
-{
+function isPalindrome(s) {
+    let check = true;
+    for (let i = 0, j = s.length - 1;
+        i < Math.floor(s.length / 2) && j >= Math.ceil(s.length / 2) && check;
+        i++ , j--) {
+        //console.log(s[i] + " " + s[j]);
+        if (s[i] == s[j]) {
+
+        } else {
+            check = false;
+        }
+
+    }
+    return check;
+
 }
+
+function main(){
+    let a = ["abba", "eye", "1234321", "luke"]
+
+    for (let i = 0; i < a.length; i++){
+        console.log("is Palindrome: " + isPalindrome(a[i]));
+    }
+}
+
+main();
