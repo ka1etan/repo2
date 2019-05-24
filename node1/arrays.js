1a877fef5cc707a32a8c790f202ccd8ff005ed84
ka1etan <ka1etan@wp.pl>
2019-05-24 17:08:49 +0200

    isPalindrome

--- node1/arrays.js
+++ node1/arrays.js
@@ -22,11 +22,69 @@ function isPalindrome(s) {
 
 }
 
+function isPalindrome2(s) {
+    let check = true;
+    for (let i = 0;
+        i < Math.floor(s.length / 2) &&  check;
+        i++ ) {
+
+        let j = s.length - 1 - i;
+        if (s[i] == s[j]) {
+
+        } else {
+            check = false;
+        }
+
+    }
+    return check;
+
+}
+
+function isPalindrome3(s) {
+    let check = true;
+    for (let i = 0, j = s.length-1;
+         i<j && check;
+        i++, j-- ) {
+
+        
+        if (s[i] == s[j]) {
+
+        } else {
+            check = false;
+        }
+
+    }
+    return check;
+
+}
+
+function isPalindrome4(s) {
+    let check = true;
+    let i = 0;
+    let j = s.length-1;
+
+    while (i<j && check ) {
+        
+        
+        if (s[i] == s[j]) {
+
+        } else {
+            check = false;
+        }
+
+        i++;
+        j--;
+
+    }
+    return check;
+
+}
+
 function main(){
     let a = ["abba", "eye", "1234321", "luke"]
 
     for (let i = 0; i < a.length; i++){
-        console.log("is Palindrome: " + isPalindrome(a[i]));
+        console.log("is Palindrome: " + isPalindrome4(a[i]));
     }
 }
 
