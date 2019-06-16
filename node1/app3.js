dd10ce21f150ae406e1368755be501b5588f7ad9
ka1etan <ka1etan@wp.pl>
2019-06-16 23:16:01 +0200

    expressJs: simple add webapp

---
+++ node1/app3.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.get('/', function (req, res) {
+    res.set('Cache-control', 'no-cache');
+    res.sendFile('d:/node_server/webapp.html');
+    // let resultCheck = req.body.add;
+    // resultCheck.addEventListener("click", app.post);
+    
+});
+
+
+
+app.post('/result', function (req, res) {
+    let input = { a: parseInt(req.body.inputBox1), b: parseInt(req.body.inputBox2)};
+    let ret = { result: input.a + input.b};
+    res.send(ret);
+});
+
+
+
+var server = app.listen(5000, function () {
+    console.log('Node server is running...');
+});
