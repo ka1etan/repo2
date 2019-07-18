e3a4ae6fbfae235a860138b1ee57929cfabcf52b
ka1etan <ka1etan@wp.pl>
2019-07-19 00:36:19 +0200

    add xhr

--- node1/app3.js
+++ node1/app3.js
@@ -39,6 +39,21 @@ app.get('/script.js', function (req, res) {
     // resultCheck.addEventListener("click", app.post);
 });
 
+app.get('/test.txt', function (req, res) {
+  var options = {
+      root: path.join(__dirname, ''),
+      // dotfiles: 'deny',
+      // headers: {
+      //   'x-timestamp': Date.now(),
+      //   'x-sent': true
+      // }
+    }    
+  res.set('Cache-control', 'no-cache');
+  res.sendFile('test.txt', options);
+  // let resultCheck = req.body.add;
+  // resultCheck.addEventListener("click", app.post);
+});
+
 app.post('/result', function (req, res) {
     let input = { a: parseInt(req.body.inputBox1), b: parseInt(req.body.inputBox2)};
     let ret = { result: input.a + input.b};
--- node1/script.js
+++ node1/script.js
@@ -1,61 +1,52 @@
 console.log("Hello script.js")
 
-function testClick()
-{
-    alert("Click");
+function testClick() {
+  alert("Click");
 }
 
-function buttons()
-{
+function buttons() {
 
-var buttonClick = document.getElementById("button1");
-buttonClick.addEventListener("click", inputData);
+  var buttonClick = document.getElementById("button1");
+  buttonClick.addEventListener("click", xhr);
 
-}
-
-function inputData()
-{
-    // let input = { a: parseInt(document.getElementById(inputBox1)), b: parseInt(document.getElementById(inputBox2))};
-    // let ret = { result: input.a + input.b};
-
-    let a = document.getElementById("inputBox1").value;
-    let b = document.getElementById("inputBox2").value;
-    //let ret = parseInt(a) + parseInt(b);
-    let ret = "tst";
-    document.getElementById("result").innerHTML = ret;
-
-    //  let xhr = new XMLHttpRequest();
-    //  xhr.open("POST", "/test.txt");
-    //  xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
-    //  xhr.send(ret);
-}
-
-function inputData2(url, data)
-{
-    
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    //xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
-    xhr.send(data);
-}
-
-function xhr()
-{
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://127.0.0.1:5000', true);
-    xhr.withCredentials = true;
-    xhr.send(null);
 }
 
 
 
-function main()
-{
-    buttons();
-    console.log("main response");
-    //inputData2("http://127.0.0.1:5000", "tst");
-     xhr();
+function xhr() {
+  var xhr = new XMLHttpRequest();
+      // xhr.open('GET', 'http://127.0.0.1:5000', true);
+      // xhr.withCredentials = true;
+      // xhr.send();
+
+  xhr.open('POST', 'http://127.0.0.1:5000');
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      document.getElementById("result").innerHTML = ret;
+      console.log("xhr");
+    }
+  }
+
+  let a = document.getElementById("inputBox1").value;
+  let b = document.getElementById("inputBox2").value;
+
+  let input = {
+    x: parseInt(a),
+    y: parseInt(b)
+  };
+  let ret = JSON.stringify({result: input.x + input.y});
+
+  xhr.send(ret);
+
+}
+
+
+
+function main() {
+  buttons();
+  console.log("main response");
+  //xhr();
 
 }
 
