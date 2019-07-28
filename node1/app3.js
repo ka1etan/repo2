4f497ee2f928939ce10dd53c46d370f337a16ccb
ka1etan <ka1etan@wp.pl>
2019-07-28 21:24:03 +0200

    app cookies v1

--- node1/app3.js
+++ node1/app3.js
@@ -1,13 +1,27 @@
+var session = require('cookie-session')
 var express = require('express');
 const path = require('path');
 var cors = require('cors');
 var app = express();
 app.use(cors({credentials: true}));
 
+var expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
 
 var bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // LR: this is what parses JSON body (application/json). See: https://www.npmjs.com/package/body-parser#expressconnect-top-level-generic
+
+app.use(session({
+  name: 'session-x',
+  keys: ['key1', 'key2'],
+  cookie: {
+    //secure: true,
+    //httpOnly: true,
+    expires: expiryDate,
+    path: '/'
+    }
+}))
 
 
 app.get('/', function (req, res) {
@@ -21,9 +35,7 @@ app.get('/', function (req, res) {
       }    
     res.set('Cache-control', 'no-cache');
     res.sendFile('webapp.html', options);
-    // let resultCheck = req.body.add;
-    // resultCheck.addEventListener("click", app.post);
-
+    res.cookie('cias', "test",  { maxAge: 500000 });
 });
 
 app.get('/script.js', function (req, res) {
@@ -44,16 +56,11 @@ app.get('/script.js', function (req, res) {
 app.get('/test.txt', function (req, res) {
   var options = {
       root: path.join(__dirname, ''),
-      // dotfiles: 'deny',
-      // headers: {
-      //   'x-timestamp': Date.now(),
-      //   'x-sent': true
-      // }
+    
     }    
   res.set('Cache-control', 'no-cache');
   res.sendFile('test.txt', options);
-  // let resultCheck = req.body.add;
-  // resultCheck.addEventListener("click", app.post);
+  
 });
 
 app.post('/result', function (req, res) {
@@ -64,6 +71,9 @@ app.post('/result', function (req, res) {
     //let ret = { result: 123.456 };
     let data = req.body;
     let result = {add: parseInt(data.a) + parseInt(data.b) }
+   // res.cookie('inputBox1', data.a,  { maxAge: 500000 });
+    res.cookie("data-a", data.a);
+    res.cookie("data-b", data.b);
     res.send(result);
 });
 
@@ -71,8 +81,17 @@ app.get('/result2', function (req, res) {
   console.log("Received: " + JSON.stringify(req.query))
   let data = req.query;
   let result = {add: parseInt(data.a) + parseInt(data.b) }
+  res.cookie("data-a", data.a);
+    res.cookie("data-b", data.b);
     res.send(result);
-   
+    
+});
+
+app.post('/cookiename', function (req, res) {
+  console.log("Received: " + JSON.stringify(req.body))
+  let data = req.body;
+  let discookie = {usernameCookie: data.name }
+  res.send(discookie);
 });
 
 // var http = require('http');
--- node1/script.js
+++ node1/script.js
@@ -17,6 +17,9 @@ function xhr() {
   var xhr = new XMLHttpRequest();
   let reqResult;
   let output;
+
+  //var expiryDate = new Date(Date.now() + 60 * 60 * 1000) 
+  //var expires = "expires="+expiryDate.toUTCString();
       // xhr.open('GET', 'http://127.0.0.1:5000', true);
       // xhr.withCredentials = true;
       // xhr.send();
@@ -29,7 +32,7 @@ function xhr() {
  // xhr.open('GET', '/result2?a');
 
 xhr.open('POST', '/result');  
-//xhr.open('GET', '/result2?a=4&b=4');
+//xhr.open('GET', '/result2?a=4&b=6');
 
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.onreadystatechange = function() {
@@ -59,17 +62,93 @@ xhr.open('POST', '/result');
 
   xhr.send(numberString);
 
-  
+  //document.cookie =  "add_data=" + numberString + ";" + expires + ";path=/";
+  document.cookie =  "data=" + numberString + ";" +  ";path=/";
+
+  //showCookies();
 
 }
 
+function showCookies()
+{
+  document.getElementById("cookies").innerHTML = document.cookie;
+}
 
+function readCookies()
+{
+  var expiryDate = new Date(Date.now() + 72 * 60 * 60 * 1000);
+  var expires = "expires="+expiryDate.toUTCString();
+
+  let name, userName, value, dcookies = {}; 
+  
+  var xhr = new XMLHttpRequest();
+  
+  acookies = document.cookie.split("; ");
+
+  for (let i =0; i < acookies.length; i++)
+  {
+      name = acookies[i].split("=")[0];
+      value = acookies[i].split("=")[1];
+      console.log("name: " + name + " , value: " + value);
+      dcookies[name] = value;
+
+  }
+
+  console.log(dcookies);
+
+  if (dcookies["name"])
+  {
+    document.getElementById("readCookie").innerHTML = dcookies["name"];
+  } 
+
+  else
+  {
+    userName = prompt("Provide your username:");
+    userNameJ = {name: userName};
+    userNameStr = JSON.stringify(userNameJ);
+
+    document.cookie =  "name=" + userName + ";" + expires + ";path=/";
+
+    xhr.open('POST', '/cookiename');  
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        document.getElementById("readCookie").innerHTML = xhr.response;
+        console.log("xhrCookie");
+       }
+    }
+    xhr.send(userNameStr);
+  }
+
+  if(dcookies["data"])
+  {
+  //document.getElementById("readCookie").innerHTML = dcookies["data-a"];
+  document.getElementById("inputBox1").value = dcookies["data-a"];
+  document.getElementById("inputBox2").value = dcookies["data-b"];
+  }
+
+ //alert(JSON.stringify(dcookies));
+
+  for (var prop in dcookies) {
+    if (prop == "data")
+    {
+      console.log("true");
+      console.log(dcookies["data-a"]);
+    }
+    else
+    {
+      console.log("false")
+    }
+}
+
+}
 
 function main() {
   buttons();
   console.log("main response");
   //xhr();
-
+  //showCookies()
+  readCookies()
 }
 
 window.addEventListener("load", main);
