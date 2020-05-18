b4db64efed9d1778c2b8ac0d21c439a472d83132
ka1etan <ka1etan@wp.pl>
2020-05-18 20:46:52 +0200

    series correction

--- node1/series.js
+++ node1/series.js
@@ -11,43 +11,39 @@
 // [1,2,3,4,5]
 //
 
-function makeSeries(a, n)
-{
+function makeSeries(a, n) {
     let series = []
     let serie = []
     let counter = 0
-    let check = a.length % n
-    
+    //let check = a.length % n
 
-    for (let i=0; i<a.length; i++){
+
+    for (let i = 0; i < a.length; i++) {
 
         serie.push(a[i])
         counter++
-        if(counter==n){
+        if (counter == n) {
             series.push(serie)
             counter = 0
             serie = []
-        if (check!==0){
-            if (n>2){
-                i=i-n+1}
-                else {
-                    i = i-1}
 
-        } else {
-
-
+            if (n > 2) {
+                i = i - n + 1
+            }
+            else {
+                i = i - 1
+            }
         }
-
-    }}
+    }
 
     console.table(series)
     return series
 }
 
-function main(){
-    let a = [1,2,3,4,5,6,7,8]
-    let n = 3
-    makeSeries(a,n)
+function main() {
+    let a = [1, 2, 3, 4, 5, 6, 7, 8]
+    let n = 4
+    makeSeries(a, n)
 }
 
 main()
