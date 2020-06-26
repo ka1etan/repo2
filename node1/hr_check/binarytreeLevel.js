408b882e8e0cf3fb0e11a87dc86f901bb01080a8
ka1etan <ka1etan@wp.pl>
2020-06-26 22:46:28 +0200

    bst level order

---
+++ node1/hr_check/binarytreeLevel.js
@@ -0,0 +1,122 @@
+// https://www.hackerrank.com/challenges/30-binary-search-trees/problem
+
+'use strict';
+
+function Node(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+}
+
+function BinarySearchTree() {
+    this.insert = function(root, data) {
+        if (root === null) {
+            this.root = new Node(data);
+            
+            return this.root;
+        }
+        
+        if (data <= root.data) {
+            if (root.left) {
+                this.insert(root.left, data);
+            } else {
+                root.left = new Node(data);
+            }
+        } else {
+            if (root.right) {
+                this.insert(root.right, data);
+            } else {
+                root.right = new Node(data);
+            }
+        }
+        
+        return this.root;
+    };
+
+    
+    
+    this.getHeight = function(root){
+
+       
+
+        if (root!==null) {
+
+            //console.log(`tree data: ${root.data}`)
+        
+        let lLength = this.getHeight(root.left)
+        let rLength = this.getHeight(root.right)
+
+        
+
+        if (lLength > rLength){
+
+            return lLength+1 
+        } else 
+        
+        {return rLength+1}
+
+    } else return 0
+        
+    }
+
+    this.levelOrder = function(root) {
+
+        
+
+        let h = this.getHeight(root)
+
+        function printLevelOrder(root, level){
+            
+            if (root==null){
+                return
+            } 
+
+                if (level==1){
+                    console.log(root.data)
+                } else if (level>1){
+                    printLevelOrder(root.left, level-1)
+                    printLevelOrder(root.right, level-1)
+                }
+            
+        }
+
+        for (let i=1; i <= h; i++){
+            printLevelOrder(root, i)
+            
+        }
+        
+
+    }
+    
+  
+}; // End of function BinarySearchTree
+
+var fs = require("fs");
+var stream = fs.createReadStream("binarytreeLevel.txt");
+
+stream.resume();
+stream.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+stream.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+stream.on('end', _ => {
+    var values = inputString.split('\n').map(Number)
+
+    //console.log(values)
+    var tree = new BinarySearchTree();
+    var root = null;
+
+    for (var i = 1; i < values.length; i++) {
+        root = tree.insert(root, values[i]);
+    }
+    
+    
+    //console.log(tree.getHeight(root));
+    tree.levelOrder(root)
+});
+
