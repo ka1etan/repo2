543d8b03501b9b68faf72d15fc72c6413e9eefd4
ka1etan <ka1etan@wp.pl>
2021-01-11 18:03:39 +0100

    sherlock corrected

--- node1/chall/sherlock.js
+++ node1/chall/sherlock.js
@@ -31,6 +31,61 @@ function readLine() {
     return inputString[currentLine++];
 }
 
+function isValid2(s) {
+
+    let letters = {}
+    let occurence = {}
+    let check = "NO"
+    let counter = 0
+    let firstProp = null
+
+    for (let i = 0; i < s.length; i++) {
+
+        if (!letters[s[i]]) {
+
+            letters[s[i]] = 1
+
+        } else {
+
+            letters[s[i]]++
+
+        }
+
+    }
+
+    for (let props in letters) {
+
+        if (!occurence[letters[props]]) {
+            occurence[letters[props]] = 1
+        } else {
+            occurence[letters[props]]++
+        }
+
+    }
+
+    for (let props in occurence) {
+
+        counter++
+
+        if (counter == 1) {
+            check = "YES"
+            firstProp = props
+        } else if (counter == 2) {
+            if (((props - firstProp == 1) && occurence[props] == 1) || (occurence[1] == 1)) {
+                check = "YES"
+            } else { check = "NO" }
+        }
+        else if (counter > 2) {
+            check = "NO"
+        }
+    }
+
+    // console.table(letters)
+    // console.table(occurence)
+
+    return check
+}
+
 function isValid(s) {
 
     let obj = {}
@@ -95,9 +150,19 @@ function main() {
 
     const s = readLine()
 
-    let result = isValid(s);
+    let result = isValid2(s);
 
     console.log(result + "\n");
     console.log(`${s}`)
 
 }
+
+// a:2
+// b:3
+// c:2
+
+
+// 2: 2
+// 3: 1
+
+// jak sa wiecej niz 2 to nie jest
